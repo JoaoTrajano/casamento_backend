633a8f68fda3e2790524bf4ae473875d0d6b1bd6
joao <joao.trajano@markup.com.br>
2022-06-18 01:14:02 -0300

    teste cors - kernel

--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,17 @@ use Illuminate\Support\Facades\Route;
  * Rotas das visitas no site
  */
 Route::prefix('register')->group(function () {
-    Route::get('/all_registers', 'RegisterController@index');
-    Route::get('/store_register', 'RegisterController@store');
+    Route::get('/all_registers', 'RegisterController@index')->middleware('cors');
+    Route::get('/store_register', 'RegisterController@store')->middleware('cors');
 });
 
 /**
  * Rotas dos comentários
  */
 Route::prefix('comments')->group(function () {
-    Route::get('/all_comments', 'CommentController@index');
-    Route::get('/get_lasts_comments', 'CommentController@getLastsComments');
-    Route::post('/store_comments', 'CommentController@store');
+    Route::get('/all_comments', 'CommentController@index')->middleware('cors');
+    Route::get('/get_lasts_comments', 'CommentController@getLastsComments')->middleware('cors');
+    Route::post('/store_comments', 'CommentController@store')->middleware('cors');
 });
 
 
